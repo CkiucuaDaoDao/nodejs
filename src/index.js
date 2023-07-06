5bc183dcf98c5fa02df6ed0bce15d965e81d0266
CkiucuaDaoDao <vinhquangtran1820@gmail.com>
2023-07-06 22:39:13 +0700

    Routes and Controllers

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 // HTTP logger
@@ -15,9 +17,9 @@ app.engine('hbs', handlebars.engine({extname: '.hbs'}));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resourses/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
+
+// route init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
